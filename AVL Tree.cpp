72cc7199578ae1a5d19986329a8b33b0fd758ee0
Sally Schaffner <schaffners@vpcc.edu>
2024-11-09 19:14:04 -0500

    Original version with no diagnostic prints

--- AVL Tree.cpp
+++ AVL Tree.cpp
@@ -30,20 +30,8 @@ Node* rightRotate(Node* y) {
     Node* x = y->left;
     Node* T2 = x->right;
 
-    cout << "\nRight rotate before rotation: " << "y =  " << y->data << " " << "x = " << y->left->data;
-    if (x->right)
-        cout << " T2 = " << x->right->data << endl;
-    else
-        cout << endl;
-
     x->right = y;
     y->left = T2;
-
-    cout << "After rotation: x-right= " << x->right->data;
-    if (y->left)
-        cout << " y->left = " << y->left->data << endl;
-    else
-        cout << endl;
 
     y->height = updateHeight(y);
     x->height = updateHeight(x);
@@ -56,20 +44,8 @@ Node* leftRotate(Node* x) {
     Node* y = x->right;
     Node* T2 = y->left;
 
-    cout << "\nLeft rotate before rotation: " << "x =  " << x->data << " " << "y = " << x->right->data;
-    if (y->left)
-        cout << " T2 = " << y->left->data << endl;
-    else
-        cout << endl;
-
     y->left = x;
     x->right = T2;
-
-    cout << "After rotation: y-left= " << y->left->data;
-    if (x->right)
-        cout << " x->right = " << T2->data << endl;
-    else
-        cout << endl;
 
     x->height = updateHeight(x);
     y->height = updateHeight(y);
@@ -91,30 +67,23 @@ Node* insert(Node* node, int key) {
     node->height = updateHeight(node);
 
     int balance = getBalanceFactor(node);
-    cout << "height: " << node->height << " balance: " << balance << endl;
 
     // Left-Left (LL) Case
-    if (balance > 1 && key < node->left->data) {
-        cout << "+++++ LL imbalance\n";
+    if (balance > 1 && key < node->left->data)
         return rightRotate(node);
-    }
-   
+
     // Right-Right (RR) Case
-    if (balance < -1 && key > node->right->data) {
-        cout << "+++++ RR imbalance\n";
+    if (balance < -1 && key > node->right->data)
         return leftRotate(node);
-    }
 
     // Left-Right (LR) Case
     if (balance > 1 && key > node->left->data) {
-        cout << "+++++ LR imbalance\n";
         node->left = leftRotate(node->left);
         return rightRotate(node);
     }
 
     // Right-Left (RL) Case
     if (balance < -1 && key < node->right->data) {
-        cout << "+++++ RL imbalance\m";
         node->right = rightRotate(node->right);
         return leftRotate(node);
     }
@@ -131,48 +100,19 @@ void inOrder(Node* root) {
     }
 }
 
-// Pre-order traversal for displaying the AVL tree
-void preOrder(Node* root) {
-    if (root) {
-        cout << root->data << " ";
-        preOrder(root->left);
-        preOrder(root->right);
-    }
-}
-
-void traversals(Node* root)
-{
-    inOrder(root);
-    cout << endl;
-    preOrder(root);
-    cout << endl << endl;
-}
-
 int main() {
     Node* root = nullptr;
 
     // Insert nodes
-    cout << "-------Inserting 10\n";
-    cout << "Before: ";
-    traversals(root);
     root = insert(root, 10);
-    cout << "After: \n";
-    traversals(root);
-    cout << "-------Inserting 20\n";
     root = insert(root, 20);
-    traversals(root);
-    cout <<  "-------Inserting 30\n";
     root = insert(root, 30);
-    traversals(root);
-    cout << "-------Inserting 40\n";
     root = insert(root, 40);
-    traversals(root);
-    cout << "-------Inserting 50\n";
     root = insert(root, 50);
-    traversals(root);
-    cout << "-------Inserting 25\n";
     root = insert(root, 25);
-    traversals(root);
+
+    cout << "In-order traversal of AVL tree: ";
+    inOrder(root);
 
     return 0;
 }
